80d5f5e96d9a3ee93511db5beb4ba1f53f7f058f
Zero <dakoolstwunn@gmail.com>
2019-05-29 01:05:58 -0400

    pip and licence

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "ifunny",
+    version = "0.1.0",
+    description = "iFunny interface in python",
+    url = "https://github.com/basswaver/iFunny",
+    download_url = "https://github.com/basswaver/iFunny/tarball/master",
+    author = "Zero",
+    author_email = "dakoolstwunn@gmail.com",
+    licence = "GPLv3",
+    classifiers = [
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords = [
+        "ifunny",
+        "ifunny-bot",
+        "bot",
+        "reverse-engineering",
+        "api",
+        "http-api",
+        "python",
+        "python3",
+        "python3.x",
+        "unofficial"
+    ],
+    install_requires = [
+        "requests"
+    ],
+    setup_requires = [
+        "wheel"
+    ],
+    packages = find_packages(),
+
+)
